3b229761e4c041855fce89803baa77ee6b24ba87
Kevin Wong <kevinxhwong@gmail.com>
2024-03-05 07:04:09 +1300

    implement login page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,5 +3,6 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+export { default as Login } from './Login';
 export { default as Signup } from './Signup';
 export { default as Landing } from './Landing';
