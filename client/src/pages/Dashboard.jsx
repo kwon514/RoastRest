5a60ed5213db4815d9a206ca3ee35457cdc1210c
Kevin Wong <kevinxhwong@gmail.com>
2024-03-05 07:11:46 +1300

    implement placeholder dashboard page

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,14 @@
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+const Dashboard = () => {
+  return (
+    <>
+      <div className="bg-bc-1">
+        <Navbar />
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
