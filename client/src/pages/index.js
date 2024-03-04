6652f7b949f40ca300e399e309e6bf28a175c799
Kevin Wong <kevinxhwong@gmail.com>
2024-03-05 07:03:42 +1300

    implement signup page

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,4 +3,5 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+export { default as Signup } from './Signup';
 export { default as Landing } from './Landing';
