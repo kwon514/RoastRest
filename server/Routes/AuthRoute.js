89116ca7e08bfbeb57ea30fb4bd4db76eb80d2a0
Kevin Wong <kevinxhwong@gmail.com>
2024-03-05 20:17:44 +1300

    implement auth-requirement middleware

--- server/Routes/AuthRoute.js
+++ server/Routes/AuthRoute.js
@@ -1,10 +1,7 @@
-const { Signup, Login } = require('../Controllers/AuthController')
-const { userVerification } = require('../Middlewares/AuthMiddleware')
 const router = require('express').Router()
+const { Signup, Login } = require('../Controllers/AuthController')
 
 router.post('/signup', Signup)
 router.post('/login', Login)
-router.post('/',userVerification)
-
 
 module.exports = router
