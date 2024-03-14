e4b1a65e4e4c7823a1816506518e3604fff01add
Kevin Wong <kevinxhwong@gmail.com>
2024-03-15 09:28:04 +1300

    update DataViewField component to display multiline values correctly

--- client/src/components/DataViewField.jsx
+++ client/src/components/DataViewField.jsx
@@ -5,7 +5,7 @@ function DataViewField({ label, value, link = false }) {
     <>
       <div className="px-0 py-2">
         <p className="text-sm">{label}:</p>
-        {link ? <a className="text-wrap break-words" href={value ? value : null} target="_blank" rel="noreferrer">{value ? value : "-"}</a> : <p className="text-md">{value ? value : "-"}</p>}
+        {link ? <a className="text-wrap break-words" href={value ? value : null} target="_blank" rel="noreferrer">{value ? value : "-"}</a> : <p className="whitespace-pre-wrap">{value ? value : "-"}</p>}
       </div>
       <Divider />
     </>
