501d41777dc1772aef648cb8e6f710e40f8882d0
Kevin Wong <kevinxhwong@gmail.com>
2024-03-26 21:13:36 +1300

    implement helper function to check if browser is logged in

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -2,3 +2,4 @@ export { default as calcRestDays } from './calcRestDays';
 export { default as parseDateToISO } from './parseDateToISO';
 export { default as getCoffeeData } from './getCoffeeData';
 export { default as isFrozen } from './isFrozen';
+export { default as isLoggedIn } from './isLoggedIn';
---
+++ client/src/helpers/isLoggedIn.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+function isLoggedIn() {
+    return axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/user`, { withCredentials: true })
+        .then((res) => {
+            return res.data
+        }).catch((err) => {
+            return err.data
+        });
+};
+
+export default isLoggedIn;
