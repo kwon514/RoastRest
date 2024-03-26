e777fc8876bff3be3ce29d641913c5082ad1d934
Kevin Wong <kevinxhwong@gmail.com>
2024-03-27 12:12:12 +1300

    implement calcRemainingDoses helper function

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -1,5 +1,8 @@
+import calcRemainingDoses from './calcRemainingDoses';
+
 export { default as calcRestDays } from './calcRestDays';
 export { default as parseDateToISO } from './parseDateToISO';
 export { default as getCoffeeData } from './getCoffeeData';
 export { default as isFrozen } from './isFrozen';
 export { default as isLoggedIn } from './isLoggedIn';
+export { default as calcRemainingDoses } from './calcRemainingDoses';
