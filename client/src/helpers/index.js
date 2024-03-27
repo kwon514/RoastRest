a98f00e6c740d6d1b0e43fffa98e43683438f2d4
Kevin Wong <kevinxhwong@gmail.com>
2024-03-28 03:06:27 +1300

    implement toTitleCase helper function

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -4,3 +4,4 @@ export { default as getCoffeeData } from './getCoffeeData';
 export { default as isFrozen } from './isFrozen';
 export { default as isLoggedIn } from './isLoggedIn';
 export { default as calcRemainingDoses } from './calcRemainingDoses';
+export { default as toTitleCase } from './toTitleCase';
