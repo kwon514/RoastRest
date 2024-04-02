e458827f0c01b1fe64e7473f2b0e74db5d2a02d2
Kevin Wong <kevinxhwong@gmail.com>
2024-04-02 21:25:57 +1300

    Add getAllCoffeeData helper function top index.js

--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -1,6 +1,7 @@
 export { default as calcRestDays } from './calcRestDays';
 export { default as parseDateToISO } from './parseDateToISO';
 export { default as getCoffeeData } from './getCoffeeData';
+export { default as getAllCoffeeData } from './getAllCoffeeData';
 export { default as isFrozen } from './isFrozen';
 export { default as isLoggedIn } from './isLoggedIn';
 export { default as calcRemainingDoses } from './calcRemainingDoses';
