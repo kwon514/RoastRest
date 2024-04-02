7ae70d9f0a82963577552a88c06b20e4696d580e
Kevin Wong <kevinxhwong@gmail.com>
2024-04-02 21:25:07 +1300

    implement search and sort functionality to coffee dashboard

--- client/src/pages/Dashboard/Dashboard.jsx
+++ client/src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,10 @@
 import { CoffeeCard, AddCoffeeDialog, ViewCoffeeDialog, EditCoffeeDialog } from './';
 import { Navbar } from 'components';
-import { isLoggedIn, getCoffeeData } from 'helpers';
-import { Grid, Fab, createTheme, ThemeProvider } from '@mui/material';
+import { isLoggedIn, getAllCoffeeData, getCoffeeData, sortCoffees } from 'helpers';
+import { Grid, Fab, createTheme, ThemeProvider, TextField, MenuItem } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
 
 const theme = createTheme({
   palette: {
@@ -32,31 +31,34 @@ const theme = createTheme({
 function Dashboard() {
   const navigate = useNavigate();
   const weightUnit = 'g';
-  const [coffeesData, setCoffeesData] = useState([]);
-  const [coffeeData, setCoffeeData] = useState({});
+  const [allCoffeeData, setAllCoffeeData] = useState([]);
+  const [visibleCoffeeData, setVisibleCoffeeData] = useState([]);
+  const [targetCoffeeData, setTargetCoffeeData] = useState({});
 
   const [addDialog, setAddDialog] = useState(false);
   const [editDialog, setEditDialog] = useState(false);
   const [viewDialog, setViewDialog] = useState(false);
 
-  const updateCoffeesData = () => {
-    axios
-      .get(`${process.env.REACT_APP_BACKEND_URL}/api/coffee`, { withCredentials: true })
-      .then((res) => {
-        setCoffeesData(res.data);
-      });
-  };
+  let currentSort = useRef('creationDate');
+  let searchBy = useRef('all');
+
+  const updateAllCoffeeData = useCallback(() => {
+    getAllCoffeeData().then((coffees) => {
+      setAllCoffeeData(coffees);
+      setVisibleCoffeeData(sortCoffees(coffees, currentSort.current));
+    });
+  }, [currentSort]);
 
   const viewCoffeeData = (id) => {
     getCoffeeData(id).then((data) => {
-      setCoffeeData(data);
+      setTargetCoffeeData(data);
       toggleViewDialog();
     });
   };
 
   const editCoffeeData = (id) => {
     getCoffeeData(id).then((data) => {
-      setCoffeeData(data);
+      setTargetCoffeeData(data);
       toggleEditDialog();
     });
   };
@@ -76,15 +78,54 @@ function Dashboard() {
     setAddDialog(!addDialog);
   };
 
+  const searchOptions = [
+    { value: 'all', label: 'All' },
+    { value: 'name', label: ' Log Name' },
+    { value: 'coffeeName', label: 'Coffee Name' },
+    { value: 'coffeeRoaster', label: 'Roaster Name' },
+  ];
+
+  const handleSetSearchBy = (value) => {
+    searchBy.current = value;
+    handleSearch(getSearchValue());
+  };
+
+  const getSearchValue = () => {
+    return document.getElementById('search').value;
+  };
+
+  const handleSearch = (value) => {
+    let searchResults = [];
+    console.log('searching by', searchBy.current, 'for', value);
+    if (searchBy.current === 'all') {
+      searchResults = allCoffeeData.filter(
+        (coffee) =>
+          coffee.name.toLowerCase().includes(value.toLowerCase()) ||
+          coffee.coffeeName.toLowerCase().includes(value.toLowerCase()) ||
+          coffee.coffeeRoaster.toLowerCase().includes(value.toLowerCase())
+      );
+    } else {
+      searchResults = allCoffeeData.filter((coffee) =>
+        coffee[searchBy.current].toLowerCase().includes(value.toLowerCase())
+      );
+    }
+    setVisibleCoffeeData(searchResults);
+  };
+
+  const handleSort = (key) => {
+    currentSort.current = key;
+    setVisibleCoffeeData(sortCoffees(visibleCoffeeData, key));
+  };
+
   useEffect(() => {
     isLoggedIn().then((res) => {
       if (res) {
-        updateCoffeesData();
+        updateAllCoffeeData();
       } else {
         navigate('/login');
       }
     });
-  }, [navigate]);
+  }, [updateAllCoffeeData, navigate]);
 
   return (
     <>
@@ -92,21 +133,67 @@ function Dashboard() {
       <ThemeProvider theme={theme}>
         <div className="max-w-screen-lg mx-auto px-3 mb-24 xl:mb-0">
           <h2 className="text-4xl text-bc-2 font-bold text-center py-8">Coffee Dashboard</h2>
+          <Grid container spacing={1} className="mb-4">
+            <Grid item xs={3} sm={2}>
+              <TextField
+                select
+                fullWidth
+                id="searchBy"
+                label="Search"
+                variant="outlined"
+                color="primary"
+                defaultValue="all"
+                onChange={(e) => handleSetSearchBy(e.target.value)}
+              >
+                {searchOptions.map((option) => (
+                  <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                  </MenuItem>
+                ))}
+              </TextField>
+            </Grid>
+            <Grid item xs={9} sm={4}>
+              <TextField
+                fullWidth
+                id="search"
+                label="Search"
+                variant="outlined"
+                color="primary"
+                onChange={(e) => handleSearch(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6} className="inline-flex justify-end">
+              <TextField
+                select
+                className="w-full sm:w-40"
+                id="sortBy"
+                label="Sort"
+                variant="outlined"
+                color="primary"
+                defaultValue="creationDate"
+                onChange={(e) => handleSort(e.target.value)}
+              >
+                <MenuItem value="creationDate">Log Date</MenuItem>
+                <MenuItem value="name">Log Name</MenuItem>
+                <MenuItem value="coffeeName">Coffee Name</MenuItem>
+                <MenuItem value="coffeeRoaster">Roaster</MenuItem>
+                <MenuItem value="coffeeWeight">Weight</MenuItem>
+                <MenuItem value="restDays">Rest Days</MenuItem>
+              </TextField>
+            </Grid>
+          </Grid>
           <Grid container spacing={2}>
-            {coffeesData
-              .slice()
-              .reverse()
-              .map((coffee) => (
-                <Grid item xs={12} sm={6} key={coffee._id}>
-                  <CoffeeCard
-                    coffeeData={coffee}
-                    weightUnit={weightUnit}
-                    viewData={viewCoffeeData}
-                    editData={editCoffeeData}
-                    updateData={updateCoffeesData}
-                  />
-                </Grid>
-              ))}
+            {visibleCoffeeData.slice().map((coffee) => (
+              <Grid item xs={12} sm={6} key={coffee._id}>
+                <CoffeeCard
+                  coffeeData={coffee}
+                  weightUnit={weightUnit}
+                  viewData={viewCoffeeData}
+                  editData={editCoffeeData}
+                  updateData={updateAllCoffeeData}
+                />
+              </Grid>
+            ))}
           </Grid>
           <Fab color="primary" aria-label="add" onClick={toggleAddDialog}>
             <AddIcon color="secondary" />
@@ -114,20 +201,20 @@ function Dashboard() {
           <AddCoffeeDialog
             open={addDialog}
             handleClose={toggleAddDialog}
-            updateData={updateCoffeesData}
+            updateData={updateAllCoffeeData}
             weightUnit={weightUnit}
           />
           <ViewCoffeeDialog
             open={viewDialog}
             handleClose={toggleViewDialog}
-            coffeeData={coffeeData}
+            coffeeData={targetCoffeeData}
             weightUnit={weightUnit}
           />
           <EditCoffeeDialog
             open={editDialog}
             handleClose={toggleEditDialog}
-            updateData={updateCoffeesData}
-            coffeeData={coffeeData}
+            updateData={updateAllCoffeeData}
+            coffeeData={targetCoffeeData}
             weightUnit={weightUnit}
           />
         </div>
