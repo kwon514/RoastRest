27ee38e0ce8fd5cb5571c977a00423f62d3ddd66
Kevin Wong <kevinxhwong@gmail.com>
2024-04-04 15:49:23 +1300

    move dialog components to Dialog subfolder

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,6 +1,6 @@
 export { default as CoffeeCard } from './CoffeeCard';
 export { default as CoffeeCardMenu } from './CoffeeCardMenu';
-export { default as AddCoffeeDialog } from './AddCoffeeDialog';
-export { default as ViewCoffeeDialog } from './ViewCoffeeDialog';
-export { default as EditCoffeeDialog } from './EditCoffeeDialog';
-export { default as DataViewField } from './DataViewField';
+export { default as AddCoffeeDialog } from './Dialogs/AddCoffeeDialog';
+export { default as ViewCoffeeDialog } from './Dialogs/ViewCoffeeDialog';
+export { default as EditCoffeeDialog } from './Dialogs/EditCoffeeDialog';
+export { default as DataViewField } from './Dialogs/DataViewField';
