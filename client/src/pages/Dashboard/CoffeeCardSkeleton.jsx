4fa7e089816707d98bf5c05267a37ff566bc207a
Kevin Wong <kevinxhwong@gmail.com>
2024-04-08 11:01:26 +1200

    change line heights of text skeletons in CoffeeCardSkeleton

--- client/src/pages/Dashboard/CoffeeCardSkeleton.jsx
+++ client/src/pages/Dashboard/CoffeeCardSkeleton.jsx
@@ -6,26 +6,26 @@ function CoffeeCardSkeleton() {
       <CardContent sx={{ padding: '8px 0 8px 16px' }}>
         <Grid container>
           <Grid item xs={12}>
-            <Skeleton variant="text" width="45%" height={40} />
-            <Skeleton variant="text" width="50%" height={20} />
+            <Skeleton variant="text" width="45%" height={36} />
+            <Skeleton variant="text" width="50%" height={25} />
           </Grid>
         </Grid>
         <Grid container spacing={1}>
           <Grid item xs={6}>
             <Skeleton variant="text" width="35%" height={20} />
-            <Skeleton variant="text" width="50%" height={20} />
+            <Skeleton variant="text" width="50%" height={25} />
           </Grid>
           <Grid item xs={6}>
             <Skeleton variant="text" width="40%" height={20} />
-            <Skeleton variant="text" width="50%" height={20} />
+            <Skeleton variant="text" width="50%" height={25} />
           </Grid>
           <Grid item xs={6}>
             <Skeleton variant="text" width="25%" height={20} />
-            <Skeleton variant="text" width="20%" height={20} />
+            <Skeleton variant="text" width="20%" height={25} />
           </Grid>
           <Grid item xs={6}>
             <Skeleton variant="text" width="50%" height={20} />
-            <Skeleton variant="text" width="20%" height={20} />
+            <Skeleton variant="text" width="20%" height={25} />
           </Grid>
         </Grid>
       </CardContent>
