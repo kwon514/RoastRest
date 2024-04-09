909e59c0eb3e99d7f70e0f6ff04edc6dbd593c51
Kevin Wong <kevinxhwong@gmail.com>
2024-04-09 18:15:04 +1200

    fix issue in isFrozen with incorrect value returned due to time of day

--- client/src/helpers/isFrozen.js
+++ client/src/helpers/isFrozen.js
@@ -1,9 +1,12 @@
 function isFrozen(frozenStart, frozenEnd) {
+  const currentDate = new Date();
+  currentDate.setHours(13, 0, 0, 0);
+
   if (!frozenStart) {
     return false;
   } else if (
     (frozenStart && !frozenEnd) ||
-    (frozenStart && frozenEnd && new Date(frozenEnd) > new Date())
+    (frozenStart && frozenEnd && new Date(frozenEnd) > currentDate)
   ) {
     return true;
   }
