49bc1e61ec24b62fa8418dc61a26e3406cf7c343
Kevin Wong <kevinxhwong@gmail.com>
2024-04-14 13:16:32 +1200

    fix parseDateToISO to return null when date is not provided

--- client/src/helpers/Miscellaneous/parseDateToISO.js
+++ client/src/helpers/Miscellaneous/parseDateToISO.js
@@ -2,7 +2,7 @@ import { parse, format } from 'date-fns';
 
 function parseDateToISO(date) {
   if (!date) {
-    return;
+    return null;
   }
   return format(parse(date, 'dd/MM/yyyy', new Date()), 'yyyy-MM-dd');
 }
