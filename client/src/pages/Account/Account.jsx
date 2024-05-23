e3e4a150c886c4e1bf438da3d480ea4dc2b9ebb1
Kevin Wong <kevinxhwong@gmail.com>
2024-05-23 12:31:45 +1200

    add theme to mui components in Account page

--- client/src/pages/Account/Account.jsx
+++ client/src/pages/Account/Account.jsx
@@ -3,7 +3,18 @@ import { isLoggedIn } from 'helpers';
 import { Helmet } from 'react-helmet';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
-import { Box, TextField, Button } from '@mui/material';
+import { createTheme, ThemeProvider, Box, TextField, Button } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#896345',
+    },
+    secondary: {
+      main: '#f0eee7',
+    },
+  },
+});
 
 function Account() {
   const navigate = useNavigate();
@@ -34,69 +45,73 @@ function Account() {
         <title>Account | RoastRest</title>
       </Helmet>
       <Navbar showLogoutButton={true} />
-      <div className="max-w-screen-lg mx-auto px-3 mb-24">
-        <h2 className="text-4xl text-rr-brown-primary font-bold text-center py-8">Your Account</h2>
-        <div className="md:w-3/4 mx-auto">
-          <Box className="bg-white p-4 rounded-md">
-            <h3 className="text-xl font-bold pb-2">Account Details</h3>
-            <TextField
-              id="name"
-              name="name"
-              label="Name"
-              defaultValue=""
-              margin="dense"
-              fullWidth
-            />
-            <TextField
-              id="email"
-              name="email"
-              label="Email"
-              defaultValue=""
-              margin="dense"
-              fullWidth
-            />
-            <Button onClick={handleAccountUpdate} color="primary" sx={{ mt: 1 }}>
-              Update Account
-            </Button>
-          </Box>
-          <Box className="bg-white p-4 rounded-md mt-4">
-            <h3 className="text-xl font-bold pb-2">Update Password</h3>
-            <TextField
-              id="password"
-              name="password"
-              label="Current Password"
-              defaultValue=""
-              margin="dense"
-              fullWidth
-            />
-            <TextField
-              id="newPassword"
-              name="newPassword"
-              label="New Password"
-              defaultValue=""
-              margin="dense"
-              fullWidth
-            />
-            <TextField
-              id="confirmNewPassword"
-              name="confirmNewPassword"
-              label="Confirm New Password"
-              defaultValue=""
-              margin="dense"
-              fullWidth
-            />
-            <Button onClick={handlePasswordUpdate} color="primary" sx={{ mt: 1 }}>
-              Update Password
-            </Button>
-          </Box>
-          <Box className="bg-white p-4 rounded-md mt-4">
-            <h3 className="text-xl font-bold pb-2">Danger Zone</h3>
-            <Button onClick={handleAccountDeletion} color="primary" sx={{ mt: 1 }}>
-              Delete Account
-            </Button>
-          </Box>
+      <ThemeProvider theme={theme}>
+        <div className="max-w-screen-lg mx-auto px-3 mb-24">
+          <h2 className="text-4xl text-rr-brown-primary font-bold text-center py-8">
+            Your Account
+          </h2>
+          <div className="md:w-3/4 mx-auto">
+            <Box className="bg-white p-4 rounded-md">
+              <h3 className="text-xl font-bold pb-2">Account Details</h3>
+              <TextField
+                id="name"
+                name="name"
+                label="Name"
+                defaultValue=""
+                margin="dense"
+                fullWidth
+              />
+              <TextField
+                id="email"
+                name="email"
+                label="Email"
+                defaultValue=""
+                margin="dense"
+                fullWidth
+              />
+              <Button onClick={handleAccountUpdate} color="primary" sx={{ mt: 1 }}>
+                Update Account
+              </Button>
+            </Box>
+            <Box className="bg-white p-4 rounded-md mt-4">
+              <h3 className="text-xl font-bold pb-2">Update Password</h3>
+              <TextField
+                id="password"
+                name="password"
+                label="Current Password"
+                defaultValue=""
+                margin="dense"
+                fullWidth
+              />
+              <TextField
+                id="newPassword"
+                name="newPassword"
+                label="New Password"
+                defaultValue=""
+                margin="dense"
+                fullWidth
+              />
+              <TextField
+                id="confirmNewPassword"
+                name="confirmNewPassword"
+                label="Confirm New Password"
+                defaultValue=""
+                margin="dense"
+                fullWidth
+              />
+              <Button onClick={handlePasswordUpdate} color="primary" sx={{ mt: 1 }}>
+                Update Password
+              </Button>
+            </Box>
+            <Box className="bg-white p-4 rounded-md mt-4">
+              <h3 className="text-xl font-bold pb-2">Danger Zone</h3>
+              <Button onClick={handleAccountDeletion} color="primary" sx={{ mt: 1 }}>
+                Delete Account
+              </Button>
+            </Box>
+          </div>
         </div>
-      </div>
+      </ThemeProvider>
     </>
   );
 }
