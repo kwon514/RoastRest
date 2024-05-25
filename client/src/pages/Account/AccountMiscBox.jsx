2f6f7b894e8fe4135aad36aee091945b5ad9b389
Kevin Wong <kevinxhwong@gmail.com>
2024-05-25 16:29:36 +1200

    change colour of delete account button to red

--- client/src/pages/Account/AccountMiscBox.jsx
+++ client/src/pages/Account/AccountMiscBox.jsx
@@ -8,7 +8,7 @@ function AccountMiscBox() {
   return (
     <Box className="bg-white p-5 rounded-md mt-4">
       <h3 className="text-xl font-bold pb-2">Danger zone</h3>
-      <Button onClick={handleAccountDeletion} variant="contained" color="primary" sx={{ mt: 2 }}>
+      <Button onClick={handleAccountDeletion} variant="contained" color="error" sx={{ mt: 2 }}>
         Delete account
       </Button>
     </Box>
