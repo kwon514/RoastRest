8653f988adf2412b6af81a33bd7bc1c6d0adf2a1
Kevin Wong <kevinxhwong@gmail.com>
2024-05-26 21:52:54 +1200

    decreased duration and hid progress bar on toast messages

--- client/src/helpers/Miscellaneous/toastMessage.js
+++ client/src/helpers/Miscellaneous/toastMessage.js
@@ -1,12 +1,13 @@
 import { toast } from 'react-toastify';
 
 function toastMessage(type, message) {
+  const toastConfig = { position: 'bottom-left', autoClose: 1500, hideProgressBar: true };
   if (type === 'success') {
-    toast.success(message, { position: 'bottom-left' });
+    toast.success(message, toastConfig);
   } else if (type === 'error') {
-    toast.error(message, { position: 'bottom-left' });
+    toast.error(message, toastConfig);
   } else {
-    toast(message, { position: 'bottom-left' });
+    toast(message, toastConfig);
   }
 }
 
