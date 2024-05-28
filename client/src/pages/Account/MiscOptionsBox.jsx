b3c85bd809430467905f0f5ae535fd8481b92916
Kevin Wong <kevinxhwong@gmail.com>
2024-05-28 19:12:47 +1200

    add toggle switch and description for direct dashboard setting

--- client/src/pages/Account/MiscOptionsBox.jsx
+++ client/src/pages/Account/MiscOptionsBox.jsx
@@ -1,9 +1,41 @@
-import { Paper } from '@mui/material';
+import { toggleSkipLanding, toastMessage } from 'helpers';
+import { Paper, Grid, FormControlLabel, Switch } from '@mui/material';
 
 function MiscOptionsBox() {
+  const initialSkipLanding = localStorage.getItem('skipLanding') === 'true';
+
+  const handleSkipLandingToggle = () => {
+    const skipLandingStatus = toggleSkipLanding();
+    toastMessage(
+      'success',
+      skipLandingStatus ? 'Landing page will now be skipped!' : 'Landing page will now be shown!'
+    );
+  };
+
   return (
     <Paper className="bg-white p-5 mt-4">
-      <h3 className="text-xl font-bold pb-2">Misc options</h3>
+      <Grid container spacing={0}>
+        <Grid item xs={10} sm={11} className="items-center flex">
+          <h3 className="text-xl font-bold">Direct dashboard</h3>
+        </Grid>
+        <Grid item xs={2} sm={1}>
+          <FormControlLabel
+            control={
+              <Switch
+                defaultChecked={initialSkipLanding}
+                onChange={handleSkipLandingToggle}
+                color="primary"
+              />
+            }
+          />
+        </Grid>
+        <Grid item xs={11} sm={12}>
+          <p>
+            Skip the landing page when you visit RoastRest and go directly to your dashboard for
+            convenience.
+          </p>
+        </Grid>
+      </Grid>
     </Paper>
   );
 }
