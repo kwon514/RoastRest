d6a75834514bfa0aa40552187d5cb3d2bca6147e
Kevin Wong <kevinxhwong@gmail.com>
2024-05-28 18:10:58 +1200

    refactor toggleSkipLanding function to use JSON.parse for localStorage value

--- client/src/helpers/Miscellaneous/toggleSkipLanding.js
+++ client/src/helpers/Miscellaneous/toggleSkipLanding.js
@@ -1,6 +1,7 @@
 function toggleSkipLanding() {
-  const skipLanding = localStorage.getItem('skipLanding');
+  const skipLanding = JSON.parse(localStorage.getItem('skipLanding'));
   localStorage.setItem('skipLanding', !skipLanding);
+  return !skipLanding;
 }
 
 export default toggleSkipLanding;
