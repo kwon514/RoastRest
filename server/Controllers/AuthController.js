181dcbd3b3012879c9abc8490e85bd2756b142c8
Kevin Wong <kevinxhwong@gmail.com>
2024-05-31 20:41:18 +1200

    refactor authentication controller to use setCookie helper function

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -21,17 +21,8 @@ module.exports.Signup = async (req, res, next) => {
     }
     const user = await User.create({ email, password, name, createdAt });
     const token = createSecretToken(user._id);
-    res.cookie('token', token, {
-      domain: process.env.DOMAIN,
-      httpOnly: true,
-      secure: true,
-      expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30 * 6),
-    });
-    res.cookie('name', user.name, {
-      domain: process.env.DOMAIN,
-      secure: true,
-      expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30 * 6),
-    });
+    setCookie(res, 'token', token, true);
+    setCookie(res, 'name', user.name, false);
     res
       .status(201)
       .json({ name: user.name, message: 'Account created successfully!', success: true });
@@ -56,17 +47,8 @@ module.exports.Login = async (req, res, next) => {
       return res.json({ message: 'Incorrect password or email.' });
     }
     const token = createSecretToken(user._id);
-    res.cookie('token', token, {
-      domain: process.env.DOMAIN,
-      httpOnly: true,
-      secure: true,
-      expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30 * 6),
-    });
-    res.cookie('name', user.name, {
-      domain: process.env.DOMAIN,
-      secure: true,
-      expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30 * 6),
-    });
+    setCookie(res, 'token', token, true);
+    setCookie(res, 'name', user.name, false);
     res.status(201).json({ name: user.name, message: 'Logged in successfully!', success: true });
     next();
   } catch (error) {
@@ -99,18 +81,8 @@ module.exports.checkAuth = async (req, res, next) => {
         const user = await User.findById(data.id);
         if (user) {
           const token = createSecretToken(user._id);
-          res.cookie('token', token, {
-            domain: process.env.DOMAIN,
-            httpOnly: true,
-            secure: true,
-            expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30 * 6),
-          });
-          res.cookie('name', user.name, {
-            domain: process.env.DOMAIN,
-            secure: true,
-            expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30 * 6),
-          });
-
+          setCookie(res, 'token', token, true);
+          setCookie(res, 'name', user.name, false);
           return res.status(200).json({ name: user.name, email: user.email });
         } else {
           return res.status(401).json({ error: 'Request is not authorized' });
