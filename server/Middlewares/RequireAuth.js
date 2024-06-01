5f3cfd58bce0be2845b31a1fb28dbab7fbcbadda
Kevin Wong <kevinxhwong@gmail.com>
2024-06-01 14:02:26 +1200

    tweak error messages in RequireAuth middleware

--- server/Middlewares/RequireAuth.js
+++ server/Middlewares/RequireAuth.js
@@ -4,16 +4,15 @@ const User = require('../Models/UserModel');
 const RequireAuth = async (req, res, next) => {
   const token = req.cookies.token;
   if (!token) {
-    return res.status(401).json({ error: 'Authorization token required' });
+    return res.status(401).json({ error: 'Authorization token required.' });
   }
-
   jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
     if (err) {
-      res.status(401).json({ error: 'Request is not authorized' });
+      res.status(401).json({ error: 'Unauthorized request.' });
     } else {
       const user = await User.findById(data.id);
       if (user) next();
-      else res.status(401).json({ error: 'Request is not authorized' });
+      else res.status(401).json({ error: 'Unauthorized request.' });
     }
   });
 };
