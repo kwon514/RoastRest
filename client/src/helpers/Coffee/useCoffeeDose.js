51456b3d32eb92b0fc6c41a5f20111509116e915
Kevin Wong <kevinxhwong@gmail.com>
2024-06-02 20:20:23 +1200

    refactor useCoffeeDose helper function

--- client/src/helpers/Coffee/useCoffeeDose.js
+++ client/src/helpers/Coffee/useCoffeeDose.js
@@ -1,15 +1,13 @@
-import axios from 'axios';
+import { editCoffeeData } from 'helpers';
 
 function useCoffeeDose(coffeeId, coffeeWeight, coffeeDose) {
   let newWeight = Math.round((coffeeWeight - coffeeDose) * 10) / 10;
 
-  if (newWeight === 0) {
-    return;
-  } else if (newWeight < 0) {
+  if (newWeight < 0) {
     newWeight = 0;
   }
 
-  return axios.put(`/coffee/${coffeeId}`, { coffeeWeight: newWeight }, { withCredentials: true });
+  return editCoffeeData(coffeeId, { coffeeWeight: newWeight });
 }
 
 export default useCoffeeDose;
