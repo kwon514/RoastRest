167f554ad8bfe39876c7725605f1c8be53fe8359
Kevin Wong <kevinxhwong@gmail.com>
2024-06-02 17:24:03 +1200

    update lastModifiedDate to current time when modifying coffee data

--- client/src/helpers/Coffee/addCoffeeData.js
+++ client/src/helpers/Coffee/addCoffeeData.js
@@ -2,10 +2,16 @@ import { parseDateToISO } from '..';
 import axios from 'axios';
 
 function addCoffeeData(data) {
+  const currentDate = new Date();
+
   data.roastDate = parseDateToISO(data.roastDate);
   data.frozenStart = parseDateToISO(data.frozenStart);
   data.frozenEnd = parseDateToISO(data.frozenEnd);
-  return axios.post(`/coffee`, { ...data, creationDate: new Date() }, { withCredentials: true });
+  return axios.post(
+    `/coffee`,
+    { ...data, lastModifiedDate: currentDate, creationDate: currentDate },
+    { withCredentials: true }
+  );
 }
 
 export default addCoffeeData;
--- client/src/helpers/Coffee/editCoffeeData.js
+++ client/src/helpers/Coffee/editCoffeeData.js
@@ -5,7 +5,11 @@ function editCoffeeData(coffeeId, data) {
   data.roastDate = parseDateToISO(data.roastDate);
   data.frozenStart = parseDateToISO(data.frozenStart);
   data.frozenEnd = parseDateToISO(data.frozenEnd);
-  return axios.put(`/coffee/${coffeeId}`, { ...data }, { withCredentials: true });
+  return axios.put(
+    `/coffee/${coffeeId}`,
+    { ...data, lastModifiedDate: new Date() },
+    { withCredentials: true }
+  );
 }
 
 export default editCoffeeData;
