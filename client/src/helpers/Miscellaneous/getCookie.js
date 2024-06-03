18d22bd422ca9584524de976d9eddfa3f31f131a
Kevin Wong <kevinxhwong@gmail.com>
2024-06-03 17:36:18 +1200

    remove unnecessary regex escape characters in getCookie helper function

--- client/src/helpers/Miscellaneous/getCookie.js
+++ client/src/helpers/Miscellaneous/getCookie.js
@@ -1,6 +1,6 @@
 function getCookie(name) {
   function escape(s) {
-    return s.replace(/([.*+?\^$(){}|\[\]\/\\])/g, '\\$1');
+    return s.replace(/([.*+?^$(){}|[\]/\\])/g, '\\$1');
   }
   var match = document.cookie.match(RegExp('(?:^|;\\s*)' + escape(name) + '=([^;]*)'));
   return match ? match[1] : null;
