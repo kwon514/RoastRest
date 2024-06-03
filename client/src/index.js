4ad00bf68887e6b59fe63add67786053558ed5de
Kevin Wong <kevinxhwong@gmail.com>
2024-06-03 17:33:26 +1200

    implement overlay scrollbar using simplebar-react package

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import SimpleBarReact from 'simplebar-react';
+import 'simplebar-react/dist/simplebar.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <SimpleBarReact style={{ height: '100vh' }}>
+      <App />
+    </SimpleBarReact>
   </BrowserRouter>
 );
