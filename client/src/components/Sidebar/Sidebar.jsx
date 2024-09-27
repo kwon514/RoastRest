a9a621e1e16a6f12e763994c90123d041bb710e3
Kevin <kevinxhwong@gmail.com>
2024-09-27 21:57:05 +1200

    refactor Sidebar to use SidebarItem component

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
-import { Drawer, Grid2 as Grid } from '@mui/material';
+import { Drawer } from '@mui/material';
 import { Dashboard, PersonOutlineOutlined } from '@mui/icons-material';
-import { Link } from 'react-router-dom';
+import { SidebarItem } from 'components';
 function Sidebar() {
   return (
     <>
@@ -12,26 +12,8 @@ function Sidebar() {
         }}
       >
         <div className="mt-24">
-          <Link to="/dashboard">
-            <div className="px-4 py-2 hover:bg-slate-200">
-              <Grid container>
-                <Grid size={2}>
-                  <Dashboard />
-                </Grid>
-                <Grid size={10}>Dashboard</Grid>
-              </Grid>
-            </div>
-          </Link>
-          <Link to="/account">
-            <div className="px-4 py-2 hover:bg-slate-200">
-              <Grid container>
-                <Grid size={2}>
-                  <PersonOutlineOutlined />
-                </Grid>
-                <Grid size={10}>Account</Grid>
-              </Grid>
-            </div>
-          </Link>
+          <SidebarItem icon={<Dashboard />} label={'Dashboard'} link={'/dashboard'} />
+          <SidebarItem icon={<PersonOutlineOutlined />} label={'Account'} link={'/account'} />
         </div>
       </Drawer>
     </>
