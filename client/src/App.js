8f48de1bdce8ce2f84c548e0a90ceb92d47a584c
Kevin <kevinxhwong@gmail.com>
2024-09-28 20:35:50 +1200

    defined MUI breakpoints for consistency with tailwindCSS

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,15 @@ const theme = createTheme({
       main: '#f0eee7',
     },
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+    },
+  },
   components: {
     MuiFab: {
       styleOverrides: {
