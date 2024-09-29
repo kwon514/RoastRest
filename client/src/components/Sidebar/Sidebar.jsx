2dffab768d1109a78a2e3fba7c0b90a623a6ac0c
Kevin <kevinxhwong@gmail.com>
2024-09-29 16:21:55 +1300

    added logo and header to temporary side drawer

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { Drawer } from '@mui/material';
 import { Dashboard, PersonOutlineOutlined } from '@mui/icons-material';
+import roastrest_logo from 'assets/roastrest_icon.png';
 import { SidebarItem } from 'components';
 
 function Sidebar({ mobileSidebar, setMobileSidebar }) {
@@ -31,7 +32,13 @@ function Sidebar({ mobileSidebar, setMobileSidebar }) {
           display: { sm: 'none' },
         }}
       >
-        <div className="mt-24 pr-2">
+        <div className="flex items-center ml-4 mt-4">
+          <span className="inline-flex items-center">
+            <img className="mr-2" src={roastrest_logo} alt="RoastRest logo" width="38px" />
+            <span className="text-xl font-semibold">RoastRest</span>
+          </span>
+        </div>
+        <div className="mt-4 pr-2">
           <SidebarItem
             icon={<Dashboard />}
             label={'Dashboard'}
