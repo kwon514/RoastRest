95681a7658c703cd5ee73e48584d2902d708c1de
Kevin <kevinxhwong@gmail.com>
2024-10-05 13:33:33 +1300

    add support for filters in getCoffee endpoint method

--- server/Controllers/CoffeeController.js
+++ server/Controllers/CoffeeController.js
@@ -15,7 +15,9 @@ module.exports.addCoffee = async (req, res) => {
 module.exports.getCoffee = async (req, res) => {
   try {
     const userId = jwt.verify(req.cookies.token, process.env.TOKEN_KEY).id;
-    const coffee = await Coffee.find({ userId, isBinned: false });
+    const filters = req.query;
+    filters.userId = userId;
+    const coffee = await Coffee.find(filters);
     res.status(200).json(coffee);
   } catch (error) {
     console.error(error);
