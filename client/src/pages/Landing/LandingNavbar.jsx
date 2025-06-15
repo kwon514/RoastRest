80b0693ebc814a1e46b2fccbe1415a202566fcf0
Kevin <kevinxhwong@gmail.com>
2025-06-15 19:41:49 +1200

    refactor LandingNavbar layout and add nav links section

--- client/src/pages/Landing/LandingNavbar.jsx
+++ client/src/pages/Landing/LandingNavbar.jsx
@@ -18,21 +18,20 @@ function LandingNavbar({ showNavMenu, mobileSidebar, setMobileSidebar }) {
       position="sticky"
       sx={{
         zIndex: 1250,
-        bgcolor: 'white',
+        bgcolor: '#fbf8f3',
         color: 'black',
-        padding: '12px',
+        padding: '30px 12px 0 12px',
         boxShadow: 0,
-        borderBottom: '1px rgb(229, 231, 235) solid',
       }}
     >
       <Grid container>
-        <Grid size={6}>
+        <Grid size={{ xs: 4, sm: 4, md: 4, lg: 4, xl: 3, xxl: 2.5 }} className="flex items-center">
           <Grid container>
             {showNavMenu ? (
               <MenuButton mobileSidebar={mobileSidebar} setMobileSidebar={setMobileSidebar} />
             ) : null}
             <Link to="/">
-              <div className="flex items-center ml-1 sm:ml-36">
+              <div className="flex items-center ml-5 sm:ml-12 md:ml-12 lg:ml-28 xl:ml-36">
                 <span className="inline-flex items-center">
                   <img className="mr-2" src={roastrest_logo} alt="RoastRest logo" width="45px" />
                   <span className="hidden sm:inline-flex text-2xl font-semibold">RoastRest</span>
@@ -41,8 +40,21 @@ function LandingNavbar({ showNavMenu, mobileSidebar, setMobileSidebar }) {
             </Link>
           </Grid>
         </Grid>
-        <Grid size={6} offset="auto" className="flex justify-end">
-          <div className="mr-44 mt-1">
+        <Grid size={{ xs: 4, sm: 4, md: 4, lg: 4, xxl: 7 }} className="hidden sm:flex">
+          <div className="flex items-center space-x-4 ml-0 sm:ml-6 lg:ml-8">
+            <Link to="/dashboard">
+              <StyledButton variant="text" color="inherit">
+                Dashboard
+              </StyledButton>
+            </Link>
+          </div>
+        </Grid>
+        <Grid
+          size={{ xs: 4, sm: 4, md: 4, lg: 4, xxl: 2.5 }}
+          offset="auto"
+          className="flex justify-end"
+        >
+          <div className=" mr-1 md:mr-10 lg:mr-16 xl:mr-28 2xl:mr-44 mt-1">
             <Link to="/login">
               <StyledButton variant="contained" color="primary">
                 Login
